5bcd0b8bff5f47389127a484d662ec372b91a1b0
Miki <miquelxubet@gmail.com>
2026-03-05 21:58:40 +0100

    arreglo funciones en script  css

--- AC1_Ex2_1/script.js
+++ AC1_Ex2_1/script.js
@@ -5,20 +5,22 @@ const email = document.getElementById("email");
 const confirmPassword = document.getElementById("confirm-password");
 const age = document.getElementById("age");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
+function validateUser() {
   const userNameValue = user.value.trim();
   const userError = document.getElementById("user-error");
 
   if (userNameValue === "" || userNameValue.length < 3) {
     userError.textContent = "Error: el user debe tener al menos 3 caracteres";
     user.style.border = "2px solid red";
+    return false;
   } else {
     user.style.border = "2px solid green";
     userError.textContent = "";
+    return true;
   }
+}
 
+function validateEmail() {
   const emailValue = email.value.trim();
   const emailError = document.getElementById("email-error");
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,23 +28,15 @@ form.addEventListener("submit", function (e) {
   if (!emailRegex.test(emailValue)) {
     emailError.textContent = "El formato de email no es correcto";
     email.style.border = "2px solid red";
+    return false;
   } else {
     emailError.textContent = "";
     email.style.border = "2px solid green";
+    return true;
   }
+}
 
-  const ageValue = age.value.trim();
-  const errorAge = document.getElementById("age-error");
-
-  if (ageValue < 0 || ageValue >= 100) {
-    errorAge.textContent =
-      "La edad no puede ser más pequeña que 0 ni más grande que 100";
-    age.style.border = "2px solid red";
-  } else {
-    errorAge.textContent = "";
-    age.style.border = "2px solid green";
-  }
-
+function validatePassword() {
   const passwordValue = password.value.trim();
   const passwordError = document.getElementById("error-password");
   const passwordRegex =
@@ -52,31 +46,71 @@ form.addEventListener("submit", function (e) {
     passwordError.textContent =
       "Error: el password ha de tener al menos 8 caracteres, mayúscula, minúscula, número y símbolo";
     password.style.border = "2px solid red";
+    return false;
   } else {
     passwordError.textContent = "";
     password.style.border = "2px solid green";
+    return true;
   }
+}
 
+function validateConfirmPassword() {
+  const passwordValue = password.value.trim();
   const confirmPasswordValue = confirmPassword.value.trim();
   const errorConfirmPassword = document.getElementById("error-confirmPassword");
 
   if (confirmPasswordValue !== passwordValue) {
     errorConfirmPassword.textContent = "Error: El Password no coincide";
     confirmPassword.style.border = "2px solid red";
+    return false;
   } else {
     errorConfirmPassword.textContent = "";
     confirmPassword.style.border = "2px solid green";
+    return true;
   }
+}
 
-  console.log("Username:", user.value);
-  console.log("Password:", password.value);
-  console.log("Email:", email.value);
-  console.log("confirm-password:", confirmPassword.value);
-  console.log("Age:", age.value);
+function validateAge() {
+  const ageValue = Number(age.value.trim());
+  const errorAge = document.getElementById("age-error");
 
-  user.value = "";
-  email.value = "";
-  password.value = "";
-  confirmPassword.value = "";
-  age.value = "";
+  if (ageValue < 0 || ageValue >= 1000) {
+    errorAge.textContent =
+      "La edad no puede ser más pequeña que 0 ni más grande que 100";
+    age.style.border = "2px solid red";
+    return false;
+  } else {
+    errorAge.textContent = "";
+    age.style.border = "2px solid green";
+    return true;
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userValid = validateUser();
+  const emailValid = validateEmail();
+  const passwordValid = validatePassword();
+  const confirmPasswordValid = validateConfirmPassword();
+  const ageValid = validateAge();
+
+  if (
+    userValid &&
+    emailValid &&
+    passwordValid &&
+    confirmPasswordValid &&
+    ageValid
+  ) {
+    console.log("Username:", user.value);
+    console.log("Password:", password.value);
+    console.log("Email:", email.value);
+    console.log("confirm-password:", confirmPassword.value);
+    console.log("Age:", age.value);
+
+    user.value = "";
+    email.value = "";
+    password.value = "";
+    confirmPassword.value = "";
+    age.value = "";
+  }
 });
